58d1df53849bc4afb29b147d91b4480d99b7e24b
Alp Eren Yalcin <95969634+yalcinalp@users.noreply.github.com>
2024-12-01 00:55:42 +0300

    Add files via upload

---
+++ farm.c
@@ -0,0 +1,111 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned getval_289()
+{
+    return 3285616968U;
+}
+
+unsigned getval_351()
+{
+    return 3354888520U;
+}
+
+unsigned getval_196()
+{
+    return 3352725832U;
+}
+
+void setval_347(unsigned *p)
+{
+    *p = 3287779656U;
+}
+
+unsigned getval_383()
+{
+    return 3456190669U;
+}
+
+void setval_154(unsigned *p)
+{
+    *p = 3263777968U;
+}
+
+unsigned getval_406()
+{
+    return 3284306248U;
+}
+
+unsigned addval_447(unsigned x)
+{
+    return x + 4152969385U;
+}
+
+unsigned addval_310(unsigned x)
+{
+    return x + 2425394264U;
+}
+
+unsigned getval_424()
+{
+    return 2428668234U;
+}
+
+unsigned addval_170(unsigned x)
+{
+    return x + 2429978696U;
+}
+
+unsigned getval_352()
+{
+    return 3285057866U;
+}
+
+unsigned addval_354(unsigned x)
+{
+    return x + 3599321098U;
+}
+
+void setval_135(unsigned *p)
+{
+    *p = 3287777608U;
+}
+
+unsigned addval_399(unsigned x)
+{
+    return x + 2425444426U;
+}
+
+unsigned addval_437(unsigned x)
+{
+    return x + 3352166728U;
+}
+
+unsigned getval_189()
+{
+    return 3285057864U;
+}
+
+unsigned addval_114(unsigned x)
+{
+    return x + 4005793880U;
+}
+
+unsigned addval_338(unsigned x)
+{
+    return x + 1488860422U;
+}
+
+void setval_280(unsigned *p)
+{
+    *p = 3263777014U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
